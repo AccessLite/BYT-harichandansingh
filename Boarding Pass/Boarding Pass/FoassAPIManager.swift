5c44fa5c920760f9bc23e8cfac25695626227b1e
harichandansingh <singh.harichandan@gmail.com>
2016-11-23 02:15:07 -0500

    Completed FoassAPIManager build

--- Boarding Pass/Boarding Pass/FoassAPIManager.swift
+++ Boarding Pass/Boarding Pass/FoassAPIManager.swift
@@ -11,6 +11,11 @@ import Foundation
 class FoassAPIManager {
     //MARK: - Properties
     private static let defaultSession = URLSession(configuration: .default)
+    private static let name: String = "Louis"
+    private static var foassURL: URL {
+        return URL(string: "https://www.foaas.com/awesome/\(name)")!
+    }
+    private static let operationsURL: URL = URL(string: "https://www.foaas.com/operations")!
     
     //MARK: - Methods
     internal class func getFoass(url: URL, completion: @escaping (Foass?) -> Void) {
@@ -32,6 +37,26 @@ class FoassAPIManager {
             }
             catch {
                 print("Encountered an error when casting from JSON: \(error)")
+            }
+            }.resume()
+    }
+    
+    internal class func getOperations(completion: @escaping ([FoassOperation]?)->Void ) {
+        defaultSession.dataTask(with: FoassAPIManager.operationsURL) { (data: Data?, response: URLResponse?, error: Error?) in
+            if error != nil {
+                print("Failed data task for [FoassOperation?]: \(error)")
+            }
+            var allOperations:[FoassOperation] =  []
+            do {
+                guard let operationsArray = try JSONSerialization.jsonObject(with: data!, options: []) as? [[String: AnyObject]] else { return }
+                for dict in operationsArray {
+                    let operation = FoassOperation(json: dict)
+                    allOperations.append(operation!)
+                    completion(allOperations)
+                }
+            }
+            catch {
+                print("Encountered an error while getting [FoassOperation]?: \(error)")
             }
             }.resume()
     }
